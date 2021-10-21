5be026e60aac01ff68564528d24b72808427afec
jhhong0509 <jhhong0509@naver.com>
2021-10-21 09:23:00 +0900

    :gear: :: 기본 세팅

---
+++ src/test/java/com/projectflow/projectflow/ProjectFlowSocketioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectflow.projectflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFlowSocketioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
