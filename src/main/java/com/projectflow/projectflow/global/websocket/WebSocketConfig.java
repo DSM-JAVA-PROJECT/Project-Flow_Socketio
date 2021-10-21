c122491db31cfd3cf7b20a1d7872fd38400325c4
jhhong0509 <jhhong0509@naver.com>
2021-10-22 08:53:31 +0900

    :recycle: :: 필요없는 주석/import 삭제

--- src/main/java/com/projectflow/projectflow/global/websocket/WebSocketConfig.java
+++ src/main/java/com/projectflow/projectflow/global/websocket/WebSocketConfig.java
@@ -1,17 +1,12 @@
 package com.projectflow.projectflow.global.websocket;
 
 import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.projectflow.projectflow.domain.chat.payload.ChatRequest;
 import com.projectflow.projectflow.global.websocket.security.WebSocketConnectController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PreDestroy;
 
 @RequiredArgsConstructor
 @Component
@@ -33,10 +28,5 @@ public class WebSocketConfig {
         server.addConnectListener(connectController::onConnect);
         return server;
     }
-
-//    @PreDestroy
-//    public void socketStop() {
-//        server.stop();
-//    }
 
 }
