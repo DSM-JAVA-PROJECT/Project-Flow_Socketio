4b17cea75d634381441193e38a9effb3c6f6a1ed
jhhong0509 <jhhong0509@naver.com>
2021-10-22 08:50:51 +0900

    ⚡️ :: LocalDateTime Serialization을 위한 의존성 추가

---
+++ src/main/java/com/projectflow/projectflow/domain/chat/message/MessageService.java
@@ -0,0 +1,2 @@
+package com.projectflow.projectflow.domain.chat.message;public interface MessageService {
+}
---
+++ src/main/java/com/projectflow/projectflow/global/objcetmapper/ObjectMapperModule.java
@@ -0,0 +1,2 @@
+package com.projectflow.projectflow.global.objcetmapper;public class ObjectMapperModule {
+}
