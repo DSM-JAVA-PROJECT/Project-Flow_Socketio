c56e59cd14071477fa154cb89597dc10da4f4ab2
jhhong0509 <jhhong0509@naver.com>
2021-11-04 08:47:38 +0900

    :bug: :: endpoint 실수 수정

--- src/main/java/com/projectflow/projectflow/domain/project/controller/ProjectController.java
+++ src/main/java/com/projectflow/projectflow/domain/project/controller/ProjectController.java
@@ -5,10 +5,12 @@ import com.projectflow.projectflow.domain.project.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/project")
+@RequestMapping("/project")
+@RestController
 public class ProjectController {
 
     private final MemberService memberService;
