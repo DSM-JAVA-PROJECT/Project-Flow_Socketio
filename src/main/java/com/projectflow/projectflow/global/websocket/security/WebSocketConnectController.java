529b0b5aeff34acd02b534c3cbcc23cf87a2fb7e
jhhong0509 <jhhong0509@naver.com>
2021-11-03 14:32:09 +0900

    ⚡️ :: 파라미터에서 토큰 가져오도록 변경

--- src/main/java/com/projectflow/projectflow/global/websocket/security/WebSocketConnectController.java
+++ src/main/java/com/projectflow/projectflow/global/websocket/security/WebSocketConnectController.java
@@ -1,11 +1,7 @@
 package com.projectflow.projectflow.global.websocket.security;
 
 import com.corundumstudio.socketio.SocketIOClient;
-import com.projectflow.projectflow.global.security.exceptions.JwtExpiredException;
-import com.projectflow.projectflow.global.security.exceptions.JwtValidatingFailedException;
 import com.projectflow.projectflow.global.security.httpsecurity.JwtTokenValidator;
-import com.projectflow.projectflow.global.websocket.security.exceptions.SocketJwtExpiredException;
-import com.projectflow.projectflow.global.websocket.security.exceptions.SocketJwtValidatingFailedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -18,15 +14,7 @@ public class WebSocketConnectController {
 
     public void onConnect(SocketIOClient client) {
         String token = client.getHandshakeData().getSingleUrlParam("Authorization");
-
-        try {
-            Authentication authentication = validator.createAuthentication(token);
-            client.set("userInfo", authentication.getName());
-        } catch (JwtValidatingFailedException e) {
-            throw SocketJwtValidatingFailedException.EXCEPTION;
-        } catch (JwtExpiredException e) {
-            throw SocketJwtExpiredException.EXCEPTION;
-        }
-
+        Authentication authentication = validator.createAuthentication(token);
+        client.set("userInfo", authentication.getName());
     }
 }
