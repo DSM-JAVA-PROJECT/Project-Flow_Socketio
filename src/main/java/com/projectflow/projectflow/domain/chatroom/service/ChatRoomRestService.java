8b7c58970ffd97a39b24a16621ee7b1d9aa625bd
jhhong0509 <jhhong0509@naver.com>
2021-11-17 08:08:37 +0900

    ⚡️ :: 채팅방 이름 변경 로직 추가

--- src/main/java/com/projectflow/projectflow/domain/chatroom/service/ChatRoomRestService.java
+++ src/main/java/com/projectflow/projectflow/domain/chatroom/service/ChatRoomRestService.java
@@ -2,9 +2,14 @@ package com.projectflow.projectflow.domain.chatroom.service;
 
 import com.projectflow.projectflow.domain.chatroom.payload.ChatMemberListResponse;
 import com.projectflow.projectflow.domain.chatroom.payload.ChatRoomListResponse;
+import com.projectflow.projectflow.domain.chatroom.payload.ChatRoomNameRequest;
 
 public interface ChatRoomRestService {
     ChatMemberListResponse getChatRoomMember(String chatRoomId);
 
     ChatRoomListResponse getChatRooms(String projectId);
+
+    void updateChatRoomImage(String chatRoomId, String imageUrl);
+
+    void updateChatRoomName(String chatRoomId, ChatRoomNameRequest request);
 }
