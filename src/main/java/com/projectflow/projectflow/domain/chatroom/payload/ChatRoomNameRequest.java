a634f840230716c2d6d3aa7c0adcf8ba4eba244e
jhhong0509 <jhhong0509@naver.com>
2021-11-17 08:07:17 +0900

    ⚡️ :: 채팅방 이름 변경 Request 추가

---
+++ src/main/java/com/projectflow/projectflow/domain/chatroom/payload/ChatRoomNameRequest.java
@@ -0,0 +1,14 @@
+package com.projectflow.projectflow.domain.chatroom.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomNameRequest {
+
+    private String name;
+
+}
