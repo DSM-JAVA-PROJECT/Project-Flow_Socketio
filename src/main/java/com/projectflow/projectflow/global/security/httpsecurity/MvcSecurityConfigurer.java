e867b2f7e260c88117481ad6de77810978b9ad5d
jhhong0509 <jhhong0509@naver.com>
2021-11-19 23:32:28 +0900

    ⚡️ :: 테스트

--- src/main/java/com/projectflow/projectflow/global/security/httpsecurity/MvcSecurityConfigurer.java
+++ src/main/java/com/projectflow/projectflow/global/security/httpsecurity/MvcSecurityConfigurer.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsUtils;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class MvcSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenValidator validator;
